a3cd18120e04d5189adea3ddd1d2d52309a6d769
RishiHubG <n.rishi@gmail.com>
2021-12-08 16:45:18 +0530

    Updates

---
+++ Filters.sql
@@ -0,0 +1,163 @@
+	--DECLARE @inputJSON VARCHAR(MAX)=N'{"viewName":"Testing Filtesr","EntityTypeId":9,"EntityId":-1,"ParentEntityTypeId":1,"ParentEntityId":40,"viewId":-1,"viewType":1,"filtersData":{"matchCondition":-200,"filters":[{"columnId":"14","colDataType":"textfield","colKey":"contactname","conditionId":"3","items":[],"noOfValuesRequired":1,"value1":"Haz"},{"columnId":"-5","colDataType":"datetime","colKey":"DateCreated","conditionId":"55","value1":"","value2":"","items":[],"noOfValuesRequired":1},{"columnId":"-6","colDataType":"datetime","colKey":"Datemodified","conditionId":"56","value1":"","value2":"","items":[],"noOfValuesRequired":2},{"columnId":"-200","colDataType":"all","colKey":"all","conditionId":-1,"value1":"","value2":"","items":[{"columnId":"13","colDataType":"select","colKey":"levelOfAutomation","conditionId":"21","value1":"b","value2":"","items":[],"noOfValuesRequired":1}]},{"columnId":"12","colDataType":"select","colKey":"causalSubCategory","conditionId":"14","value1":"b","value2":"","items":[],"noOfValuesRequired":1},{"columnId":"16","colDataType":"checkbox","colKey":"notify","conditionId":70,"value1":"True","value2":"","items":[]},{"columnId":"13","colDataType":"select","colKey":"levelOfAutomation","conditionId":"12","value1":"b","value2":"","items":[],"noOfValuesRequired":1}],"currentUser":false,"topRecords":"ALL","orderByColumn":"","sortBy":"desc"},"columns":[{"colName":"Component - Weighted Audit Error %","colId":"componentweightedauditerror","isSelected":1,"orderid":1},{"colName":"Component Name","colId":"name","isSelected":1,"orderid":2},{"colName":"Component Weight","colId":"componentweight","isSelected":1,"orderid":3},{"colName":"Overall Weight","colId":"overallweight","isSelected":1,"orderid":4},{"colName":"Test Error","colId":"testerror","isSelected":1,"orderid":5},{"colName":"Total Errors","colId":"totalerrors","isSelected":false,"orderid":6},{"colName":"Total Sample Size","colId":"totalsamplesize","isSelected":1,"orderid":7}]}'
+
+	DECLARE @inputJSON VARCHAR(MAX)=N'{"viewName":"Testing Filtesr","EntityTypeId":9,"EntityId":-1,"ParentEntityTypeId":1,"ParentEntityId":40,"viewId":-1,"viewType":1,"filtersData":{"matchCondition":-200,"filters":[{"columnId":"14","colDataType":"textfield","colKey":"contactname","conditionId":"3","items":[],"noOfValuesRequired":1,"value1":"Haz"},{"columnId":"-5","colDataType":"datetime","colKey":"DateCreated","conditionId":"55","value1":"","value2":"","items":[],"noOfValuesRequired":1},{"columnId":"-6","colDataType":"datetime","colKey":"Datemodified","conditionId":"56","value1":"","value2":"","items":[],"noOfValuesRequired":2},{"columnId":"-200","colDataType":"all","colKey":"all","conditionId":-1,"value1":"","value2":"","items":[{"columnId":"13","colDataType":"select","colKey":"levelOfAutomation","conditionId":"21","value1":"b","value2":"","items":[],"noOfValuesRequired":1},{"columnId":"13","colDataType":"select","colKey":"levelOfAutomation1","conditionId":"21","value1":"b1","value2":"","items":[],"noOfValuesRequired":1}]},{"columnId":"12","colDataType":"select","colKey":"causalSubCategory","conditionId":"14","value1":"b","value2":"","items":[],"noOfValuesRequired":1},{"columnId":"16","colDataType":"checkbox","colKey":"notify","conditionId":70,"value1":"True","value2":"","items":[]},{"columnId":"13","colDataType":"select","colKey":"levelOfAutomation","conditionId":"12","value1":"b","value2":"","items":[],"noOfValuesRequired":1}],"currentUser":false,"topRecords":"ALL","orderByColumn":"","sortBy":"desc"},"columns":[{"colName":"Component - Weighted Audit Error %","colId":"componentweightedauditerror","isSelected":1,"orderid":1},{"colName":"Component Name","colId":"name","isSelected":1,"orderid":2},{"colName":"Component Weight","colId":"componentweight","isSelected":1,"orderid":3},{"colName":"Overall Weight","colId":"overallweight","isSelected":1,"orderid":4},{"colName":"Test Error","colId":"testerror","isSelected":1,"orderid":5},{"colName":"Total Errors","colId":"totalerrors","isSelected":false,"orderid":6},{"colName":"Total Sample Size","colId":"totalsamplesize","isSelected":1,"orderid":7}]}'
+	
+	DROP TABLE IF EXISTS #TMP_ALLSTEPS
+	DROP TABLE IF EXISTS #TMP_FiltersData
+	DROP TABLE IF EXISTS #TMP_FiltersWithMatchCondition
+
+	SELECT *
+			INTO #TMP_ALLSTEPS
+	 FROM dbo.HierarchyFromJSON(@inputJSON) 
+
+	 --SELECT * FROM #TMP_ALLSTEPS
+
+	 ;WITH CTE_FiltersData
+			AS
+			(		
+				SELECT T.Element_ID,
+					   T.Name AS ColumnName, 
+					   T.Parent_ID,	   
+					   T.StringValue,
+					   T.ValueType
+				 FROM #TMP_ALLSTEPS T			  
+				 WHERE Name ='filtersData'
+
+				 UNION ALL
+
+				 SELECT T.Element_ID,
+					   T.Name, 
+					   T.Parent_ID,			   
+					   T.StringValue,
+					   T.ValueType
+				 FROM CTE_FiltersData C
+					  INNER JOIN #TMP_ALLSTEPS T ON T.Parent_ID = C.Element_ID
+		
+			)
+
+			SELECT * 
+				INTO #TMP_FiltersData
+			FROM CTE_FiltersData --WHERE ValueType ='boolean' AND  StringValue = 'true'
+
+			SELECT * FROM #TMP_FiltersData ORDER BY ELEMENT_ID--WHERE ColumnName IN ('colKey','conditionId','value1','value2') ORDER BY Element_ID
+
+			SELECT Parent_ID,
+				   MAX(CASE WHEN ColumnName = 'colKey' THEN StringValue END) AS colKey,
+				   MAX(CASE WHEN ColumnName = 'conditionId' THEN StringValue END) AS conditionId,
+				   MAX(CASE WHEN ColumnName = 'value1' THEN StringValue END) AS value1,
+				   MAX(CASE WHEN ColumnName = 'value2' THEN StringValue END) AS value2,
+				   CAST(NULL AS VARCHAR(50)) AS MatchCondition,
+				   CAST(NULL AS VARCHAR(50)) AS SubMatchCondition
+				INTO #TMP_FiltersWithMatchCondition
+			FROM #TMP_FiltersData
+			--WHERE StringValue IS NOT NULL
+			--	  AND StringValue <> 'all'
+			GROUP BY Parent_ID
+
+			DELETE FROM #TMP_FiltersWithMatchCondition
+			WHERE colKey IS NULL
+				  OR colKey ='all'
+
+			DECLARE @matchCondition VARCHAR(10) = (SELECT CASE WHEN StringValue = -200 THEN 'AND' ELSE 'OR' END FROM #TMP_FiltersData WHERE ColumnName ='matchCondition')
+			--SELECT @matchCondition
+
+			UPDATE #TMP_FiltersWithMatchCondition SET MatchCondition = @matchCondition
+
+			SELECT * FROM #TMP_FiltersWithMatchCondition
+
+			--SELECT Parent_ID							
+			--FROM #TMP_FiltersData TMP
+			--	 CROSS APPLY (
+			--					SELECT StringValue
+			--					FROM #TMP_FiltersData 
+			--					WHERE Parent_ID = TMP.Parent_ID
+			--						  AND 
+				 
+			--				)
+			--WHERE ColumnName IN ('colKey','conditionId','value1','value2') 
+			--GROUP BY Parent_ID
+			 
+
+		DROP TABLE IF EXISTS #TMP_ItemsWithMatchCondition
+
+		;WITH CTE_ItemsFiltersData
+			AS
+			(		
+				SELECT T.Element_ID,
+					   T.ColumnName, 
+					   T.Parent_ID,	   
+					   T.StringValue,
+					   T.ValueType
+				 FROM #TMP_FiltersData T			  
+				 WHERE ColumnName ='items'
+
+				 UNION ALL
+
+				 SELECT T.Element_ID,
+					   T.ColumnName, 
+					   T.Parent_ID,			   
+					   T.StringValue,
+					   T.ValueType
+				 FROM CTE_ItemsFiltersData C
+					  INNER JOIN #TMP_FiltersData T ON T.Parent_ID = C.Element_ID
+		
+			)
+
+			--SELECT *				 
+			--FROM CTE_ItemsFiltersData 
+
+			SELECT Parent_ID,
+				   MAX(CASE WHEN ColumnName = 'colKey' THEN StringValue END) AS colKey,
+				   MAX(CASE WHEN ColumnName = 'conditionId' THEN StringValue END) AS conditionId,
+				   MAX(CASE WHEN ColumnName = 'value1' THEN StringValue END) AS value1,
+				   MAX(CASE WHEN ColumnName = 'value2' THEN StringValue END) AS value2,
+				   CAST(NULL AS VARCHAR(50)) AS MatchCondition,
+				   CAST(NULL AS INT) AS ItemID
+				INTO #TMP_ItemsWithMatchCondition
+			FROM CTE_ItemsFiltersData
+			--WHERE StringValue IS NOT NULL
+			--	  AND StringValue <> 'all'
+			GROUP BY Parent_ID
+
+			DELETE FROM #TMP_ItemsWithMatchCondition WHERE colKey IS NULL
+
+			SELECT * FROM #TMP_ItemsWithMatchCondition
+
+			SELECT * FROM #TMP_FiltersData WHERE Element_ID IN (112,113)
+			SELECT * FROM #TMP_FiltersData WHERE Element_ID IN (88)
+
+			SELECT TMC.Parent_ID, T4.StringValue
+			FROM #TMP_ItemsWithMatchCondition TMC
+				 INNER JOIN #TMP_FiltersData T2 ON T2.Element_ID = TMC.Parent_ID
+				 INNER JOIN #TMP_FiltersData T3 ON T3.Element_ID = T2.Parent_ID
+				 INNER JOIN #TMP_FiltersData T4 ON T4.Parent_ID = T3.Parent_ID
+			WHERE T4.ColumnName = 'columnId'
+
+			/*ALTERNATE FOR THE ABOVE WOULD BE A RECURSIVE CTE AS BELOW': WE STILL NEED TO APPLY ONE MORE LAST JOIN/FILTER FOR ColumnName = 'columnId' TO REACH THE ABOVE RESULT
+			;WITH CTE_ItemsFiltersData
+			AS
+			(		
+				SELECT NULL AS Element_ID,
+					   T.Parent_ID,	   
+					   CAST(NULL AS VARCHAR(50)) AS StringValue,
+					   CAST(NULL AS VARCHAR(50)) AS ColumnName
+				 FROM #TMP_ItemsWithMatchCondition T
+
+				 UNION ALL
+
+				 SELECT T.Element_ID,	
+						T.Parent_ID,
+					   CAST(T.StringValue AS VARCHAR(50)),
+					   CAST(T.ColumnName AS VARCHAR(50))
+				 FROM CTE_ItemsFiltersData C
+					  INNER JOIN #TMP_FiltersData T ON T.Element_ID = C.Parent_ID				
+		
+			)
+
+			SELECT *				 
+			FROM CTE_ItemsFiltersData 
+			WHERE ColumnName = 'items'
+			*/
+
+			--UPDATE #TMP_ItemsWithMatchCondition
+			--	SET MatchCondition
--- Modules/Domain/q3.sql
+++ Modules/Domain/q3.sql
@@ -411,3 +411,38 @@ Universe',@description=N'Description',@InputJSON=N'{"domainpermissiona":[{"useri
 Auditors","read":true,"modify":false,"write":true,"cut":true,"copy":true,"delete":true,"administrate":true,"adhoc":true,"export":true,"report":true},{"userid":1,"username":"dussa","read":true,"modify":false,"write":false,"cut":false,"copy":false,"delete":false,"administrate":false,"adhoc":false,"export":true,"report":true},{"userid":1,"username":"loginid3","read":true,"modify":true,"write":true,"cut":true,"copy":true,"delete":false,"administrate":false,"adhoc":false,"export":false,"report":false}],"attributes":{"universeid":212,"exchange 
 rate":0,"currency":"inr","notes":"This is new domain creation","domain owner":"","domainowner":"ABC","exchangerate":7},"permissionList":{"jsonData":{"assigned":[{"userid":1,"username":"IT Auditors","read":true,"modify":false,"write":true,"cut":true,"copy":true,"delete":true,"administrate":true,"adhoc":true,"export":true,"report":true},{"userid":1,"username":"dussa","read":true,"modify":false,"write":false,"cut":false,"copy":false,"delete":false,"administrate":false,"adhoc":false,"export":true,"report":true},{"userid":1,"username":"loginid3","read":true,"modify":true,"write":true,"cut":true,"copy":true,"delete":false,"administrate":false,"adhoc":false,"export":false,"report":false}],"unassigned":[]}},"domianinherentpermissions":false,"frameworklist":{"1":true,"2":false,"3":false,"4":false,"5":false,"6":true,"7":true,"8":false,"9":false,"10":false,"11":false,"12":false,"13":false,"14":false,"15":false,"16":false,"17":false,"18":false,"19":false,"20":false,"21":false,"22":false,"23":false,"24":false,"25":false,"26":false,"27":false,"28":false,"29":false,"30":false,"31":false,"32":false,"33":false,"34":false,"35":false,"36":true,"37":true,"38":true,"39":true,"40":false,"41":false,"42":false,"43":false,"44":false,"45":false,"46":false,"47":false,"48":false,"49":false,"50":false,"51":false,"52":false,"53":false,"54":false,"55":false,"56":true,"57":true},"contactscontainer":{"domaincontactList":{"jsonData":{"assigned":[{"name":"test100  ","id":1014,"role":"3","notify":""},{"name":"fname mname lname","id":1017,"role":"1","notify":""},{"name":"Prashanthi Reddy Gadepally","id":1018,"role":-1,"notify":""}],"unassigned":[{"name":"Prashanthi  ","id":1012,"role":-1,"notify":""},{"name":"Srinath  ","id":1013,"role":-1,"notify":""},{"name":"agstest10  ","id":1019,"role":-1,"notify":""}]}}},"Linkcontainer":{}}',
 @MethodName=NULL,@UserLoginID=3832
+
+SELECT * FROM Filterconditions_Master
+SELECT * FROM EntityMetaData
+SELECT * FROM Registers WHERE REGISTERID=159 --GET FRAMEWORKID
+SELECT * FROM FRAMEWORKS WHERE frameworkid=40-- GET name, THIS IS THE TABLE TO APPLY FILTER ON
+SELECT * FROM FRAMEWORKS WHERE frameworkid=1-- GET name, THIS IS THE TABLE TO APPLY FILTER ON
+
+exec SaveCustomViewJSONData 
+@inputJSON=N'{"viewName":"Testing Filtesr","EntityTypeId":9,"EntityId":-1,"ParentEntityTypeId":1,"ParentEntityId":40,"viewId":-1,"viewType":1,"filtersData":{"matchCondition":-200,"filters":[{"columnId":"14","colDataType":"textfield","colKey":"contactname","conditionId":"3","items":[],"noOfValuesRequired":1,"value1":"Haz"},{"columnId":"-5","colDataType":"datetime","colKey":"DateCreated","conditionId":"55","value1":"","value2":"","items":[],"noOfValuesRequired":1},{"columnId":"-6","colDataType":"datetime","colKey":"Datemodified","conditionId":"56","value1":"","value2":"","items":[],"noOfValuesRequired":2},{"columnId":"-200","colDataType":"all","colKey":"all","conditionId":-1,"value1":"","value2":"","items":[{"columnId":"13","colDataType":"select","colKey":"levelOfAutomation","conditionId":"21","value1":"b","value2":"","items":[],"noOfValuesRequired":1}]},{"columnId":"12","colDataType":"select","colKey":"causalSubCategory","conditionId":"14","value1":"b","value2":"","items":[],"noOfValuesRequired":1},{"columnId":"16","colDataType":"checkbox","colKey":"notify","conditionId":70,"value1":"True","value2":"","items":[]},{"columnId":"13","colDataType":"select","colKey":"levelOfAutomation","conditionId":"12","value1":"b","value2":"","items":[],"noOfValuesRequired":1}],"currentUser":false,"topRecords":"ALL","orderByColumn":"","sortBy":"desc"},"columns":[{"colName":"Component - Weighted Audit Error %","colId":"componentweightedauditerror","isSelected":1,"orderid":1},{"colName":"Component Name","colId":"name","isSelected":1,"orderid":2},{"colName":"Component Weight","colId":"componentweight","isSelected":1,"orderid":3},{"colName":"Overall Weight","colId":"overallweight","isSelected":1,"orderid":4},{"colName":"Test Error","colId":"testerror","isSelected":1,"orderid":5},{"colName":"Total Errors","colId":"totalerrors","isSelected":false,"orderid":6},{"colName":"Total Sample Size","colId":"totalsamplesize","isSelected":1,"orderid":7}]}',
+@MethodName=NULL,@UserLoginID=3840
+
+select * from Filterconditions_Master WHERE criteria NOT like '%equals%'
+ALTER TABLE Filterconditions_Master ADD OperatorType VARCHAR(50)
+SELECT * FROM Filterconditions_Master
+UPDATE Filterconditions_Master SET OperatorType ='=' WHERE Criteria='Equals'
+UPDATE Filterconditions_Master SET OperatorType ='<>' WHERE Criteria='Not Equals'
+UPDATE Filterconditions_Master SET OperatorType ='LIKE ''%<COLVALUE>%''' WHERE Criteria='contains'
+UPDATE Filterconditions_Master SET OperatorType ='NOT LIKE ''%<COLVALUE>%''' WHERE Criteria='Does Not Contains'
+UPDATE Filterconditions_Master SET OperatorType ='LIKE ''%<COLVALUE>''' WHERE Criteria='Starts With'
+UPDATE Filterconditions_Master SET OperatorType ='NOT LIKE ''%<COLVALUE>''' WHERE Criteria='Does Not Start With'
+UPDATE Filterconditions_Master SET OperatorType ='LIKE ''<COLVALUE>%''' WHERE Criteria='Ends With'
+UPDATE Filterconditions_Master SET OperatorType ='NOT LIKE ''<COLVALUE>%''' WHERE Criteria='Does Not End With'
+UPDATE Filterconditions_Master SET OperatorType ='ISNULL(<COLNAME>,'''') = ''''' WHERE Criteria='Is Empty'
+UPDATE Filterconditions_Master SET OperatorType ='ISNULL(<COLNAME>,'''') <> '''' ' WHERE Criteria='IS Not Empty'
+UPDATE Filterconditions_Master SET OperatorType ='<' WHERE Criteria='Less Than'
+UPDATE Filterconditions_Master SET OperatorType ='<=' WHERE Criteria='Less Than Or Equal to'
+UPDATE Filterconditions_Master SET OperatorType ='>' WHERE Criteria='Greater Than'
+UPDATE Filterconditions_Master SET OperatorType ='>=' WHERE Criteria='Greater Than or Equal to'
+UPDATE Filterconditions_Master SET OperatorType ='Between' WHERE Criteria='Between'
+UPDATE Filterconditions_Master SET OperatorType ='Not Between' WHERE Criteria='Not Between'
+UPDATE Filterconditions_Master SET OperatorType ='1' WHERE Criteria='True'
+UPDATE Filterconditions_Master SET OperatorType ='0' WHERE Criteria='False'
+UPDATE Filterconditions_Master SET OperatorType ='ISNULL(<COLNAME>,'''') = ''''' WHERE Criteria='Empty'
+UPDATE Filterconditions_Master SET OperatorType ='ISNULL(<COLNAME>,'''') <> '''' ' WHERE Criteria='Not Empty'
+--UPDATE Filterconditions_Master SET OperatorType ='Equal to Field' WHERE Criteria='ISNULL(<COLNAME>,'''') <> '''' '
