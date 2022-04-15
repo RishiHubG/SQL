c16f651174a9f843574ca42005c42b58c59b16a9
RishiHubG <n.rishi@gmail.com>
2022-04-15 15:04:34 +0530

    Updates

--- Modules/Frameworks/Integer values save in Component.sql
+++ Modules/Frameworks/Integer values save in Component.sql
@@ -1,4 +1,7 @@
 
+TRUNCATE TABLE DemoComponent_DATA
+TRUNCATE TABLE DemoComponent_DATA_HISTORY
+
 --New Save
 exec SaveFrameworkJSONData @EntityId=-1,@EntitytypeId=9,@ParentEntityID=1415,@ParentEntityTypeID=3,
 @SpecialInputJSON=N'{"templates":[{"TemplateKey":"workingpapers","data":{"tabletemplatecontainer":{"tabletemplatedatagrid":[{"documentNae":"","workingProcedure":"","workingPaperConclusion":"","objective":"","annexureNumber":"","relevantToReport":false}]}},"dataType":"TableTemplate","apiKey":"workingPapers"}]}',
@@ -12,3 +15,12 @@ exec SaveFrameworkJSONData @EntityId=1,@EntitytypeId=9,@ParentEntityID=1415,@Par
 @transitionid=N'',
 @InputJSON=N'{"systemDescription":{"container":{"isDesignAndImplementationRequired":"Yes","AdhocsystemsampleSize":""},"systemobjective":"To ensure controls are designed and implemented effectively","systemriskAssessmsent":"","systemconclusiononoperatingeffectivenessofcontrols":"","systemnatureOfControlAndFrequencyOfPerformance":"","systemsampleSize":"","systemrevisedSampleSize":"","systemjustificationForSampleSize":"","systemdescriptionaddFindings":{"jsonData":{}}},"sampleAndWeights":{"name":"New Componen","objective":"To ensure that controls are operating effectively.","operatingeffectivenesssampleSize":"","dateUpdated":"","riskAssessmsent":"","conclusiononoperatingeffectivenessofcontrols":"","systemdescriptionrequired_1":"","natureOfControlAndFrequencyOfPerformance":"","sampleSize":"","revisedSampleSize":"","justificationForSampleSize":"","queryGrid":"","linkedFindings":{"jsonData":{}}},"adminContainer":{"knowledgebasereference":"","referencenum":"-2","inheritWorkflowEntity":"","inheritWfEntityFrameworkid":"","showAdminTab":"No","loggedInUserRole":"","loggedInUserGroup":"","isModuleAdmin":"","isSystemAdmin":"","isModuleAdminGroup":"","currentStateOwner":""},"linkSubComponents1":{"entityLinkGrid":""}}',
 @MethodName=NULL,@UserLoginID=2747
+
+SELECT * FROM DemoComponent_DATA
+
+
+
+SELECT * FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = 'DemoComponent_DATA'
+AND (DATA_TYPE LIKE '%INT%' OR DATA_TYPE LIKE '%DATE%' OR DATA_TYPE LIKE '%FLOAT%' OR DATA_TYPE LIKE '%DECIMAL%')
+
+ 
