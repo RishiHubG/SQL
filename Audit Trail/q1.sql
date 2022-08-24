40f1ada3b6199e5cd007e4bbc9c521f7e08725e9
RishiHubG <n.rishi@gmail.com>
2022-08-24 14:47:24 +0530

    Updates

--- Audit Trail/q1.sql
+++ Audit Trail/q1.sql
@@ -152,4 +152,185 @@ SELECT * FROM #TMP
 						 EXEC(@str)
 			
 			 --==========================================================================================
-			   
+
+
+
+--==========================================================================================	
+					 USE VKB_NEW
+					GO
+					DECLARE @ID INT = 1442
+					DROP TABLE IF EXISTS #TMP
+
+					SELECT IDENTITY(INT,1,1) AS ID, COLUMN_NAME, CONCAT(COLUMN_NAME,' AS NewValue, LAG(',COLUMN_NAME,')OVER(ORDER BY HISTORYID) AS OldValue', CHAR(10)) AS Col,
+							DATA_TYPE
+						INTO #TMP
+					FROM INFORMATION_SCHEMA.COLUMNS
+					WHERE TABLE_NAME = 'NewAuditFramework_data_history'
+					AND COLUMN_NAME NOT IN ('FrameworkID','HistoryID','ID','UserCreated','DateCreated','UserModified','DateModified','VersionNum','registerid','PeriodIdentifier','OperationType');
+
+					
+					UPDATE #TMP
+						SET COL = CONCAT('CAST(',REPLACE(Col,'AS NewValue',' AS NVARCHAR(MAX)) AS NewValue'))
+					WHERE DATA_TYPE IN('datetime', 'decimal','bigint','int');
+
+					UPDATE #TMP
+						SET COL = REPLACE(Col,'LAG',' CAST(LAG')
+					WHERE DATA_TYPE IN('datetime', 'decimal','bigint','int');
+
+					UPDATE #TMP
+						SET COL = REPLACE(Col,'AS OldValue',' AS NVARCHAR(MAX)) AS OldValue')
+					WHERE DATA_TYPE IN('datetime', 'decimal','bigint','int');
+
+
+					--SELECT * FROM #TMP
+
+						DECLARE @SQL VARCHAR(MAX)
+						SET @SQL = 'SELECT ID, COLUMN_NAME,tab.HistoryID,tab.DateModified, DATA_TYPE,tab.OldValue,tab.NewValue
+									FROM #TMP
+										 CROSS APPLY(SELECT TOP 100 PERCENT HistoryID,DateModified,''<Column_Name>'' AS ColName, <Col> FROM NewAuditFramework_data_history  
+																		WHERE ID=<ID> ORDER BY DateModified
+													)TAB
+									WHERE COLUMN_NAME = tab.ColName
+										AND column_name=''actualstartDate''
+									'
+						SET @SQL =  REPLACE(@SQL,'<ID>', @ID)
+
+					DROP TABLE IF EXISTS #TMP_SQL;
+
+					SELECT *,
+						REPLACE(
+								REPLACE(ColString,'<Column_Name>',COLUMN_NAME),
+								'<Col>', Col) AS strSQL
+						INTO #TMP_SQL
+					FROM #TMP
+						 CROSS APPLY(VALUES(@SQL))TAB(ColString);	
+						
+						 --SELECT STRING_AGG(strSQL, CONCAT( CHAR(10),' UNION ', CHAR(10), CHAR(10)))
+						 --FROM #TMP_SQL
+						 DROP TABLE IF EXISTS #TMPHistData;
+
+						 CREATE TABLE #TMPHistData(ID INT, Column_Name VARCHAR(500),HistoryID INT, DateModified datetime2(6),Data_Type VARCHAR(50),OldValue NVARCHAR(MAX),NewValue NVARCHAR(MAX))
+
+						 DECLARE @STR VARCHAR(MAX)
+						 SELECT @str = STRING_AGG(strSQL, CONCAT( CHAR(10),' UNION ', CHAR(10), CHAR(10)))
+						 FROM #TMP_SQL
+
+						 PRINT @STR
+
+						 INSERT INTO #TMPHistData(ID,Column_Name,HistoryID,DateModified,Data_Type,OldValue,NewValue)
+							EXEC(@str)	
+								
+								
+								 ;WITH CTE
+								 AS
+								 (
+								   SELECT *, ROW_NUMBER()OVER(PARTITION BY Column_Name,DateModified Order By OldValue) AS RowNum
+								   FROM #TMPHistData
+								 )
+								 SELECT * FROM CTE
+			
+			 --==========================================================================================
+			 
+	
+
+--==========================================================================================	
+						 USE VKB_NEW
+						GO
+					DECLARE @ID INT = 1442
+					DROP TABLE IF EXISTS #TMP
+
+					DECLARE @TableName VARCHAR(500) = 'NewAuditFramework_data_history'
+
+					SELECT IDENTITY(INT,1,1) AS ID, COLUMN_NAME, CONCAT(COLUMN_NAME,' AS NewValue, LAG(',COLUMN_NAME,')OVER(ORDER BY HISTORYID) AS OldValue', CHAR(10)) AS Col,
+							DATA_TYPE
+						INTO #TMP
+					FROM INFORMATION_SCHEMA.COLUMNS
+					WHERE TABLE_NAME = @TableName
+					AND COLUMN_NAME NOT IN ('FrameworkID','HistoryID','ID','UserCreated','DateCreated','UserModified','DateModified','VersionNum','registerid','PeriodIdentifier','OperationType');
+
+					DECLARE @strDT VARCHAR(500) = 'CONVERT(NVARCHAR(MAX),<ColName>,20)'
+					
+					UPDATE #TMP
+						SET COL = REPLACE(Col,COLUMN_NAME,REPLACE(@strDT,'<ColName>', COLUMN_NAME))
+					WHERE DATA_TYPE = 'datetime'
+					 
+					UPDATE #TMP
+						SET COL = CONCAT('CAST(',REPLACE(Col,'AS NewValue',' AS NVARCHAR(MAX)) AS NewValue'))
+					WHERE DATA_TYPE IN('decimal','bigint','int');
+
+					UPDATE #TMP
+						SET COL = REPLACE(Col,'LAG',' CAST(LAG')
+					WHERE DATA_TYPE IN('decimal','bigint','int');
+
+					UPDATE #TMP
+						SET COL = REPLACE(Col,'AS OldValue',' AS NVARCHAR(MAX)) AS OldValue')
+					WHERE DATA_TYPE IN('decimal','bigint','int');
+
+ 
+						DECLARE @SQL VARCHAR(MAX)
+						SET @SQL = 'SELECT ID, COLUMN_NAME,tab.OldHistoryID,tab.NewHistoryID,tab.DateModified, DATA_TYPE,tab.OldValue,tab.NewValue
+									FROM #TMP
+										 CROSS APPLY(SELECT TOP 100 PERCENT HistoryID AS NewHistoryID,LAG(HISTORYID)OVER(ORDER BY HISTORYID) AS OldHistoryID,
+																DateModified,''<Column_Name>'' AS ColName, <Col> FROM <TableName> 
+																		WHERE ID=<ID> ORDER BY DateModified
+													)TAB
+									WHERE COLUMN_NAME = tab.ColName
+										--AND column_name=''actualstartDate''
+									'
+						SET @SQL =  REPLACE(@SQL,'<ID>', @ID)
+						SET @SQL =  REPLACE(@SQL,'<TableName>', @TableName)
+
+					DROP TABLE IF EXISTS #TMP_SQL;
+
+					SELECT *,
+						REPLACE(
+								REPLACE(ColString,'<Column_Name>',COLUMN_NAME),
+								'<Col>', Col) AS strSQL
+						INTO #TMP_SQL
+					FROM #TMP
+						 CROSS APPLY(VALUES(@SQL))TAB(ColString);	
+						 
+						 --SELECT STRING_AGG(strSQL, CONCAT( CHAR(10),' UNION ', CHAR(10), CHAR(10)))
+						 --FROM #TMP_SQL
+						 DROP TABLE IF EXISTS #TMPHistData;
+
+						 CREATE TABLE #TMPHistData(ID INT, Column_Name VARCHAR(500),OldHistoryID INT,NewHistoryID INT, DateModified datetime2(6),Data_Type VARCHAR(50),OldValue NVARCHAR(MAX),NewValue NVARCHAR(MAX))
+
+						 DECLARE @STR VARCHAR(MAX)
+						 SELECT @str = STRING_AGG(strSQL, CONCAT( CHAR(10),' UNION ', CHAR(10), CHAR(10)))
+						 FROM #TMP_SQL
+
+						 PRINT @STR
+
+						 INSERT INTO #TMPHistData(ID,Column_Name,OldHistoryID,NewHistoryID,DateModified,Data_Type,OldValue,NewValue)
+							EXEC(@str)	
+								
+								
+								 ;WITH CTE
+								 AS
+								 (
+								   SELECT *--, ROW_NUMBER()OVER(PARTITION BY Column_Name,DateModified Order By OldValue) AS RowNum
+								   FROM #TMPHistData
+								   WHERE OldHistoryID IS NOT NULL
+								 )
+								 SELECT * FROM CTE
+								 WHERE ISNULL(OldValue,-1) <> ISNULL(NewValue,-1)
+								 ORDER BY NewHistoryID
+			
+			 --==========================================================================================
+
+		
+
+			
+
+			 USE VKB_NEW
+			 GO
+
+			  SELECT frameworkid FROM Registers WHERE registerid = 1
+
+ 
+	SELECT *
+	FROM dbo.Frameworks 
+	WHERE FrameworkID = 6
+
+	SELECT auditOjectives, auditStatus, actualcompletiondate,actualstartdate,* FROM NewAuditFramework_data_history WHERE ID=1442
